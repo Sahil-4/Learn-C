b4b062e70de8c1d4e7daa0a593b517b65d29bdda
Sahil-4 <www.sahil.875020@gmail.com>
2023-07-26 18:15:54 +0530

    fixed issues and mistakes in C007-Strings

--- C007-Strings/Strings.c
+++ C007-Strings/Strings.c
@@ -1,35 +1,33 @@
-// Strings.c 
 
 #include <stdio.h>
 
-// Strings : 1D Character Array Terminated by a null Character 
-// null Character : End of String ('\0')
 
 int main() {
+    // Strings : 1D Character Array Terminated by a null Character 
+    // null Character : End of String ('\0')
+
 	// syntax :
 	// Declaration and Assignment of String 
-	// char Str[] = {"A", "l", "p", "h", "a", "\0"};
+	// char str[] = {"A", "l", "p", "h", "a", "\0"};
 	// \0 is Useful to Terminate String 
 
 	// another way 
-	// char Str[] = "Alpha"; // this will automatically convert into above string by C Compiler 
+	char str[] = "Alpha"; // this will automatically convert into above string by C Compiler 
+
 	// Accessing and Printing String 
-	//  printf("%c\n", Str[0]);
-
+	// printf("%c\n", str[0]);
     // Another way 
-    // printf("%s\n", Str);
+    printf("%s\n", str);
 
-/*
     // Print String with while loop and Pointer 
-    char *Ptr = Str;
+    char *Ptr = str;
     while (Ptr != '\0') {
         printf("%c", *Ptr);
         Ptr++;
     }
-*/
+
 
     // taking user input 
-
     char firstname[10], lastname[10];
     // scanf("%s", firstname);
     // printf("%s\n", firstname);
@@ -38,12 +36,12 @@ int main() {
     // printf("%s\n", lastname);
 
     // with this method we can only take a single string 
-    // lets take a string that includes spaces 
+    // lets take input of a string that includes spaces 
     char fullname[20];
     // gets(&fullname);
     // puts(&fullname);
     // printf("%s", fullname);
+
+
 	return 0;
 }
-
-
